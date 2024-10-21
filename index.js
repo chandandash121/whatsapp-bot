01c7ebc74ba5ff40fbc78ff5cbd48d636d76d9eb
chandandash121 <chandandash164@gmail.com>
2024-10-22 02:08:27 +0530

    Updated webhook endpoint

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { MessagingResponse } = require('twilio').twiml;
 const xlsx = require('xlsx');
-const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,35 +16,9 @@ app.post('/whatsapp', (req, res) => {
 
     console.log(`Received message from ${fromNumber}: ${incomingMessage}`);
 
-    // Split the incoming message by commas
-    const data = incomingMessage.split(',').map(item => item.trim());
-
-    if (data.length !== 4) {
-        const twiml = new MessagingResponse();
-        twiml.message('Please send the data in the format: Name, Age, Email, Phone');
-        return res.end(twiml.toString());
-    }
-
-    const [name, age, email, phone] = data;
-
-    // Create a new workbook and a worksheet
-    const workbook = xlsx.utils.book_new();
-    const worksheetData = [
-        ['Name', 'Age', 'Email', 'Phone'], // Column headers
-        [name, age, email, phone],          // Data row
-    ];
-    const worksheet = xlsx.utils.aoa_to_sheet(worksheetData);
-
-    // Append the worksheet to the workbook
-    xlsx.utils.book_append_sheet(workbook, worksheet, 'Contacts');
-
-    // Save the workbook to a file
-    const filePath = `./${name.replace(/\s+/g, '_')}_contact.xlsx`; // Create a filename
-    xlsx.writeFile(workbook, filePath);
-
-    // Reply to the user
+    // Respond to the user (for testing)
     const twiml = new MessagingResponse();
-    twiml.message(`Your data has been processed and saved to ${filePath}!`);
+    twiml.message('Your message was received!');
     
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twiml.toString());
